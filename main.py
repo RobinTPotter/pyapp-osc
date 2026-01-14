ac7dc4817940f9d7dfe8aefb38d7f63712b851f7
Potter, Robin <Robin.Potter@experian.com>
2026-01-14 21:16:51 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 
@@ -8,8 +9,24 @@ from kivy.uix.boxlayout import BoxLayout
 
 class OSC(App):
     def build(self):
-        self.connect("192.168.1.174", 57120)
+        self.ip = "192.168.1.174"
+        self.port = 57120
+        self.connect(self.ip, self.port)
         root = BoxLayout(orientation="vertical")
+
+
+        very_top = BoxLayout(orientation="horizontal", size_hint_y=0.05)
+        self.ip_text = TextInput(text=self.ip, multiline=False)
+        self.port_text = TextInput(text=str(self.port), multiline=False)
+        setme = Button(text="Set")
+        setme.bind(on_press=self.set_connect)
+
+        very_top.add_widget(self.ip_text)
+        very_top.add_widget(self.port_text)
+        very_top.add_widget(setme)
+        root.add_widget(very_top)
+
+
         top = BoxLayout(orientation="horizontal", size_hint_y=0.6)
         b1 = Button(text="/voice/sine")
         b1.bind(on_press=self.on_button)
@@ -30,6 +47,12 @@ class OSC(App):
         root.add_widget(bottom)
         return root
 
+    def set_connect(self, button):
+        self.ip = self.ip_text.text
+        self.port = int(self.port_text.text)
+        self.connect(self.ip, self.port)
+        
+
     def on_button(self, button):
         print(f"hi {button.text}")
         o = self.c.send_message(f"{button.text}", "")
@@ -38,6 +61,7 @@ class OSC(App):
     def connect(self, ip="0.0.0.0", port=8080):
         from pythonosc.udp_client import SimpleUDPClient
         self.c = SimpleUDPClient(ip, port)
+        print(self.c)
 
 
 if __name__ == '__main__':
